c0c5286ee3ec6acf2ca1fe6ade91d3a6968d5d0f
Anneloes <anneloes9405@hotmail.com>
2020-05-12 19:21:37 -0400

    button now comes with animate effect and sound
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- game.js
+++ game.js
@@ -1,4 +1,31 @@
+
+let gamePattern = []
+const buttonColours = ["red", "blue", "green", "yellow"]
+
 function nextSequence() {
+  /* 
+    1. CREATE ARRAY
+    Each turn, a colour is picked through selecting a random number (0 - 3)
+    This colour is then added to the gamePattern array
+  */
   let randomNumber = Math.floor(Math.random() * 4)
-  console.log(randomNumber)
+
+  let randomChosenColour = buttonColours[randomNumber]
+  
+  console.log(randomChosenColour)
+  
+  gamePattern.push(randomChosenColour)
+  
+  /* 
+    2. CREATE EFFECT
+    the button creates an animate ffect and plays a sound after click
+  */
+  
+  $(`#${randomChosenColour}`).animate({opacity: 0.5}).animate({opacity: 1})
+  
+  let colourSoundEffect = new Audio(`${randomChosenColour}.mp3`)
+  colourSoundEffect.play()
 }
+
+nextSequence()
+
