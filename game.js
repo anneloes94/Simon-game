f668f47c2f95508a2ee2689e0cb109a231c79123
Anneloes <anneloes9405@hotmail.com>
2020-05-12 20:26:01 -0400

    some refactoring
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- game.js
+++ game.js
@@ -1,6 +1,21 @@
 
 let gamePattern = []
+let userClickedPattern = []
+
 const buttonColours = ["red", "blue", "green", "yellow"]
+
+/*
+  3. USER SELECTS COLOURS
+  the users selects a colour, which gets added to the userClickedPattern array
+*/
+
+
+$('.btn').click(function() {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour)
+  // console.log(userClickedPattern)
+  playSound(userChosenColour)
+})
 
 function nextSequence() {
   /* 
@@ -9,23 +24,27 @@ function nextSequence() {
     This colour is then added to the gamePattern array
   */
   let randomNumber = Math.floor(Math.random() * 4)
-
   let randomChosenColour = buttonColours[randomNumber]
-  
-  console.log(randomChosenColour)
-  
   gamePattern.push(randomChosenColour)
   
   /* 
     2. CREATE EFFECT
     the button creates an animate ffect and plays a sound after click
   */
-  
   $(`#${randomChosenColour}`).animate({opacity: 0.5}).animate({opacity: 1})
   
-  let colourSoundEffect = new Audio(`${randomChosenColour}.mp3`)
+  playSound(randomChosenColour)
+  
+
+
+  
+}
+
+function playSound(name) {
+  let colourSoundEffect = new Audio(`sounds/${name}.mp3`)
   colourSoundEffect.play()
 }
 
-nextSequence()
+
+
 
