dbd60f17635407f2f8dcc3a75d3cc971728eab8a
Anneloes <anneloes9405@hotmail.com>
2020-05-15 13:21:03 -0400

    on wrong button press, wrong answer class is added in css
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 
 let gamePattern = []
 let userClickedPattern = []
+
 let level = 0
 let started = false
 
@@ -24,8 +25,31 @@ $('.btn').click(function() {
   checkAnswer(userClickedPattern.length - 1)
 })
 
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence()
+      }, 1000)
+    }
+  } else {
+    playSound("wrong")
+    $("body").addClass("game-over")
+
+    setTimeout(function(){
+      $("body").removeClass("game-over")
+    }, 200)
+    $("#level-title").html(`Game Over, Press Any Key to Restart`)
+    
+    startOver()
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = []
+
   level++
+  $("#level-title").html(`Level ${level}`)
 
   let randomNumber = Math.floor(Math.random() * 4)
   let randomChosenColour = buttonColours[randomNumber]
@@ -36,13 +60,10 @@ function nextSequence() {
  playSound(randomChosenColour)  
 }
 
-function checkAnswer(currentLevel) {
-  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
-    console.log(`${userClickedPattern[currentLevel]} - ${gamePattern[currentLevel]}`)
-    console.log("right")
-  } else {
-    console.log("wrong")
-  }
+function startOver() {
+  level = 0
+  gamePattern = []
+  started = false
 }
 
 function playSound(name) {
